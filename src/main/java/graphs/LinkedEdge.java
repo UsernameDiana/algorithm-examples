03659af66d81f894df55350420fe2f94f50c3d0b
diana <diana@10.50.130.18>
2017-03-17 12:07:59 +0100

    starting on LinkedGraph

--- src/main/java/graphs/LinkedEdge.java
+++ src/main/java/graphs/LinkedEdge.java
@@ -3,7 +3,17 @@ package graphs;
 import graphs.Graph.Edge;
 import graphs.Graph.Vertex;
 
-public class LinkedEdge implements Edge<D,W> {
+public class LinkedEdge<D,W> implements Edge<D,W> {
     W weight;
     Vertex<D,W> headVertex;
+
+    @Override
+    public W getWeight() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Vertex<D, W> getHeadVertex() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
--- src/main/java/graphs/LinkedVertex.java
+++ src/main/java/graphs/LinkedVertex.java
@@ -4,8 +4,18 @@ import graphs.Graph.Edge;
 import graphs.Graph.Vertex;
 import java.util.Collection;
 
-public class LinkedVertex implements Vertex<D,W> {
+public class LinkedVertex<D,W> implements Vertex<D,W> {
     D data;
     Collection <Edge<D,W>> edges;
+
+    @Override
+    public D getData() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<Edge<D, W>> getAdjacentEdges() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
