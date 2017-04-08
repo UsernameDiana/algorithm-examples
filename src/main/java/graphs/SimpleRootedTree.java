e7f28de2ecb303bcc5a93c718118b02d676ccada
diana <diana@10.50.130.18>
2017-04-08 14:18:24 +0200

    must finish SimpleRootedTree, SearchGraph

--- src/main/java/graphs/SimpleRootedTree.java
+++ src/main/java/graphs/SimpleRootedTree.java
@@ -6,69 +6,86 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class SimpleRootedTree<D,W> implements RootedTree<D, W>{
-  private final Graph<D,W> graph;
-  private final Vertex<D,W> root;
-  private final Set<Edge<D,W>> edges = new HashSet<>();
+public class SimpleRootedTree<D, W> implements RootedTree<D, W> {
 
-  public SimpleRootedTree(Graph<D,W> graph, Vertex<D,W> root) {
-    this.graph = graph;
-    this.root = root;
+    private final Graph<D, W> graph;
+    private final Vertex<D, W> root;
+    private final Set<Edge<D, W>> edges = new HashSet<>();
+
+    public SimpleRootedTree(Graph<D, W> graph, Vertex<D, W> root) {
+        this.graph = graph;
+        this.root = root;
     }
 
-  @Override
-  public Vertex<D, W> getRoot() {
-    return root;
+    @Override
+    public Vertex<D, W> getRoot() {
+        return root;
     }
 
-  @Override
-  public void add(Edge<D, W> edge) {
-    edges.add(edge);
+    @Override
+    public void add(Edge<D, W> edge) {
+        edges.add(edge);
     }
 
-  @Override
-  public void addVertex(D... data) {
-    
-  }
+    @Override
+    public void addVertex(D... data) {
+        Vertex v;
+        // TODO
+    }
+    private int myNumEdges;
 
-  @Override
-  public void addEdge(W weight, Vertex<D, W> tail, Vertex<D, W> head, boolean undirected) {
-    
-  }
-
-  @Override
-  public Vertex<D, W> vertexOf(D data) {
-    return graph.vertexOf(data);
+    @Override
+    public void addEdge(W weight, Vertex<D, W> tail, Vertex<D, W> head, boolean undirected) {
+        // TODO
+        Vertex v, w;
+//        if (hasEdge(head, tail)) {
+//            return;  // no duplicate edges
+//        }
+//        myNumEdges += 1;
+//        if ((v = getVertex(head)) == null) {
+//            v = addVertex(head);
+//        }
+//        if ((w = getVertex(tail)) == null) {
+//            w = addVertex(tail);
+//        }
+//        myAdjList.get(v).add(w);
+//        myAdjList.get(w).add(v);  // undirected graph only
     }
 
-  @Override
-  public Collection<Vertex<D, W>> getVertices() {
-    return graph.getVertices();
+    @Override
+    public Vertex<D, W> vertexOf(D data) {
+        return graph.vertexOf(data);
     }
 
-  @Override
-  public Collection<Edge<D, W>> getEdges() {
-    return edges;
+    @Override
+    public Collection<Vertex<D, W>> getVertices() {
+        return graph.getVertices();
     }
 
-  public Collection<Edge<D, W>> getEdgesFrom(Vertex<D, W> vertex) {
-    // This piece of code simply filters away edges
-    // that is not in the edges set.
-    // Unfortunately Java has to convert to stream, and back to set.
-    return graph.getEdges().stream()
-        .filter(e -> edges.contains(e))
-        .collect(Collectors.toSet());
+    @Override
+    public Collection<Edge<D, W>> getEdges() {
+        return edges;
     }
 
-  @Override
-  public void print(PrintStream out) {
-    print(out, "", root);
+    public Collection<Edge<D, W>> getEdgesFrom(Vertex<D, W> vertex) {
+        // This piece of code simply filters away edges
+        // that is not in the edges set.
+        // Unfortunately Java has to convert to stream, and back to set.
+        return graph.getEdges().stream()
+                .filter(e -> edges.contains(e))
+                .collect(Collectors.toSet());
     }
-  
-  private void print(PrintStream out, String indent, Vertex<D,W> vertex) {
-    out.println(indent+vertex.getData());
-    for (Edge<D,W> edge : getEdgesFrom(vertex))
-        print(out, indent+"  ", edge.getHeadVertex());
+
+    @Override
+    public void print(PrintStream out) {
+        print(out, "Vertex root: ", root);
     }
-  
-  }
+
+    private void print(PrintStream out, String indent, Vertex<D, W> vertex) {
+        out.println(indent + vertex.getData());
+        for (Edge<D, W> edge : getEdgesFrom(vertex)) {
+            print(out, indent + "  ", edge.getHeadVertex());
+        }
+    }
+
+}
